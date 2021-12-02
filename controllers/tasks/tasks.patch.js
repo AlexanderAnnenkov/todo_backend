d664b5b6b7afd5002a43ee24789f36f7b27e1330
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-02 16:04:35 +0300

    Add method "PATCH" for edit task name and done

--- controllers/tasks/tasks.patch.js
+++ controllers/tasks/tasks.patch.js
@@ -2,10 +2,11 @@ import db from "../../db.json"
 import fs from "fs"
 
 export default (req, res) => {
-  if (req.body.name) {
     const editTask = db.find((e) => e.uuid == req.params.id)
     editTask.name = req.body.name
+    editTask.done = req.body.done
     fs.writeFileSync("db.json", JSON.stringify(db))
     res.send(editTask)
-  }
+  
+  
 }
