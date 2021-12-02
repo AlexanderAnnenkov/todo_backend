e00537861dbc16cc5fade64076e1a7dc9312fb43
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-02 14:43:37 +0300

    Flag 'Done' automatic create false

--- controllers/tasks/tasks.post.js
+++ controllers/tasks/tasks.post.js
@@ -3,14 +3,12 @@ import fs from 'fs'
 
 export default (req, res) => {
   
-  if(!req.body) return res.send('123');
+  if(!req.body) return res.sendStatus(400);
 
-  
-  const taskName = req.body.name
-  const taskDone = req.body.done
-  let task = { name: taskName, done: taskDone }
+  const task = { name: req.body.name}
 
   // увеличиваем его на единицу
+  task.done = false
   task.uuid = Math.floor(Math.random() * 10000)
   task.date = new Date()
   // добавляем пользователя в массив
