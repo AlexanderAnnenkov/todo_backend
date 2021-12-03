43c52a1511a6e2ecf0e69e113b04d24c2336cea1
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-03 15:32:00 +0300

    fix response

--- controllers/tasks/tasks.delete.js
+++ controllers/tasks/tasks.delete.js
@@ -5,5 +5,5 @@ export default (req, res) => {
   const deleteItem = db.tasks.filter(e => e.uuid != req.params.id)
   db.tasks = deleteItem
   fs.writeFileSync("db.json", JSON.stringify(db))
-  res.send(deleteItem)
+  res.send({ data: "Success Delete" })
 }
