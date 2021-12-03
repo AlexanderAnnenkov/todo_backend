15bbd6907e507fc2f51f9471d080e0e241ddfe88
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-03 13:37:57 +0300

    Add check on title has 1 symbol , and switch generate uuid

--- controllers/tasks/tasks.post.js
+++ controllers/tasks/tasks.post.js
@@ -2,14 +2,17 @@ import db from "../../db.json"
 import fs from "fs"
 
 export default (req, res) => {
-  if (!req.body) return res.sendStatus(400)
+  try {
+    if (!req.body) throw { message: "Bad Request" }
+    if (req.body.name.length < 2) throw { message: "Title need has 2 symbols" }
 
-  const task = { name: req.body.name }
+    const task = { name: req.body.name }
 
-  task.done = false
-  task.uuid = Math.floor(Math.random() * 10000)
-  task.date = new Date()
-  db.push(task)
-  fs.writeFileSync("db.json", JSON.stringify(db))
-  res.send(task)
+    task.done = false
+    task.uuid = Math.floor(Math.random() * 10000)
+    task.date = new Date()
+    db.push(task)
+    fs.writeFileSync("db.json", JSON.stringify(db))
+    res.send(task)
+  } catch (err) {}
 }
