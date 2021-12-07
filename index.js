a7d1899fbca59d6a8efde0e90c00293aea638203
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-07 14:08:42 +0300

    Delete unused imports

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors")
-const fs = require('fs')
-const router = require("./routes/router")
-
-dotenv.config()
-
+const fs = require("fs")
 
 const PORT = 3001
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 fs.readdirSync("./routes").forEach((file) => {
-      app.use("/", require(`./routes/${file}`))
-  })
+  app.use("/", require(`./routes/${file}`))
+})
 
 app.listen(PORT, () => {})
