0b9c19dfa83d3f86b99ae790c7b8433f7e695c80
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-15 12:09:18 +0300

    Add check name task unique

--- routes/tasks/tasks.post.js
+++ routes/tasks/tasks.post.js
@@ -1,10 +1,14 @@
-const models = require("../../models/").User
+const models = require("../../models/")
 const express = require("express")
 const router = express.Router()
 
 module.exports = router.post("/task", async (req, res, next) => {
   try {
-    const user = await models.findByPk(req.user.id)
+    const user = await models.User.findByPk(req.user.id)
+    const checkTask = await models.Task.findOne({
+      where: { name: req.body.name, userId: req.user.id },
+    })
+    if (checkTask) throw new Error('Name must be unique')
 
     const task = await user.createTask({ name: req.body.name })
 
