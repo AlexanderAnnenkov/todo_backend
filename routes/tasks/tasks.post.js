5250df654d7b79917d17dfb643071537974149c6
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-16 17:16:03 +0300

    Add index for table "Tasks"

--- routes/tasks/tasks.post.js
+++ routes/tasks/tasks.post.js
@@ -8,9 +8,15 @@ module.exports = router.post("/task", async (req, res, next) => {
     const checkTask = await models.Task.findOne({
       where: { name: req.body.name, userId: req.user.id },
     })
+    
     if (checkTask) throw new Error('Name must be unique')
-
-    const task = await user.createTask({ name: req.body.name })
+    const allTask = await models.Task.findAll({where:{ userId:req.user.id}})
+    const taskIndex= allTask.map((e) => e.index)
+    const arrIndex = Math.max.apply(null, taskIndex)
+    let index
+    arrIndex !== -Infinity ? index = arrIndex + 1 : index = 1
+    
+    const task = await user.createTask({ name: req.body.name, index: index})
 
     res.send(task)
   } catch (err) {
