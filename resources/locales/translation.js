10b8707e5a811597b656997ebd94eda722c626d5
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-20 18:35:23 +0300

    Add dictionary

---
+++ resources/locales/translation.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ru: {
+    errNameTask: "Название задачи должно быть больше 2 символов",
+    errNameTaskUnique: "Название задачи должно быть уникальным",
+    errDeleteTask: "У вас нет доступа к этой задаче",
+  },
+  en: {
+    errNameTask: "Name task need more 2 symbols",
+    errNameTaskUnique: "Name must be unique",
+    errDeleteTask: "You don`t root on task",
+  },
+  ua: {
+    errNameTask: "Назва завдання має бути більше 2 символів",
+    errNameTaskUnique: "Назва завдання має бути унікальною",
+    errDeleteTask: "У вас немає доступу до цього завдання",
+  },
+}
